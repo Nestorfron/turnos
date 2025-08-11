8506f06639d6562f35cc12d44cb1157e8ebe4327
Nestorfron <nestorfrones07@hotmail.com>
2025-08-11 07:13:08 -0300

    PWA

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,18 @@ import App from "./App";
 import { AppProvider } from "./context/AppContext";
 import './styles/index.css';
 
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("Hay una nueva versión disponible. ¿Quieres actualizar?")) {
+      updateSW();
+    }
+  },
+  onOfflineReady() {
+    console.log("La aplicación está lista para funcionar offline 🚀");
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
